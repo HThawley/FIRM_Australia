7606017d130fff745e0e33223046e3fe46004b55
TimWeberRE100 <timothy.weber@anu.edu.au>
2023-08-17 10:51:22 +1000

    corred zones

--- run.sh
+++ run.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
 
 
-python3 Optimisation.py -s 11 -z [0] -n 25 -v 0
-python3 Optimisation.py -s 11 -z [1] -n 25 -v 0
-python3 Optimisation.py -s 11 -z [2] -n 25 -v 0
-python3 Optimisation.py -s 11 -z [3] -n 25 -v 0
-python3 Optimisation.py -s 11 -z [4] -n 25 -v 0
-python3 Optimisation.py -s 11 -z [5] -n 25 -v 0
-python3 Optimisation.py -s 11 -z [6] -n 25 -v 0
-python3 Optimisation.py -s 11 -z [7] -n 25 -v 0
-python3 Optimisation.py -s 11 -z all -n 25 -v 0
+python3 Optimisation.py -s 21 -z '[3 24 25]' -n 25 -v 0
+python3 Optimisation.py -s 21 -z '[3 24 25]' -n 10 -v 0
 
+python3 Optimisation.py -s 21 -z '[15 23 25]' -n 25 -v 0
+python3 Optimisation.py -s 21 -z '[15 23 25]' -n 10 -v 0
 
-
-
-python3 Optimisation.py -s 21 -z [0] -n 25 -v 0
-python3 Optimisation.py -s 21 -z [1] -n 25 -v 0
-python3 Optimisation.py -s 21 -z [2] -n 25 -v 0
-python3 Optimisation.py -s 21 -z [3] -n 25 -v 0
-python3 Optimisation.py -s 21 -z all -n 25 -v 0
-
+python3 Optimisation.py -s 21 -z '[6 14]' -n 25 -v 0
+python3 Optimisation.py -s 21 -z '[6 14]' -n 10 -v 0
