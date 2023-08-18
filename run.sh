ceb43003f2300402353b49aed274c1bce388da68
TimWeberRE100 <timothy.weber@anu.edu.au>
2023-08-18 20:06:03 +1000

    More results

--- run.sh
+++ run.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
+python3 Optimisation.py -s 21 -z '[7]' -n 25 -v 0
+python3 Optimisation.py -s 21 -z '[10]' -n 25 -v 0
+python3 Optimisation.py -s 21 -z '[14]' -n 25 -v 0
+python3 Optimisation.py -s 21 -z '[3 27 28]' -n 25 -v 0
+python3 Optimisation.py -s 21 -z '[18 26]' -n 25 -v 0
+python3 Optimisation.py -s 21 -z '[18 28]' -n 25 -v 0
+python3 Optimisation.py -s 21 -z '[18 26 28]' -n 25 -v 0
+python3 Optimisation.py -s 21 -z '[3 13 27]' -n 25 -v 0
 
-python3 Optimisation.py -s 21 -z '[3 24 25]' -n 25 -v 0
-python3 Optimisation.py -s 21 -z '[3 24 25]' -n 10 -v 0
+python3 Optimisation.py -s 21 -z all -n 10 -v 0 -i 800
+python3 Optimisation.py -s 21 -z all -n 25 -v 0 -i 800
 
-python3 Optimisation.py -s 21 -z '[15 23 25]' -n 25 -v 0
-python3 Optimisation.py -s 21 -z '[15 23 25]' -n 10 -v 0
+python3 Optimisation.py -s 22 -z '[7]' -n 25 -v 0
+python3 Optimisation.py -s 22 -z '[10]' -n 25 -v 0
+python3 Optimisation.py -s 22 -z '[14]' -n 25 -v 0
+python3 Optimisation.py -s 22 -z '[29]' -n 25 -v 0
+python3 Optimisation.py -s 22 -z '[31]' -n 25 -v 0
+python3 Optimisation.py -s 22 -z '[3 27 28]' -n 25 -v 0
+python3 Optimisation.py -s 22 -z '[18 26]' -n 25 -v 0
+python3 Optimisation.py -s 22 -z '[18 28]' -n 25 -v 0
+python3 Optimisation.py -s 22 -z '[18 26 28]' -n 25 -v 0
+python3 Optimisation.py -s 22 -z '[3 13 27]' -n 25 -v 0
+python3 Optimisation.py -s 22 -z all -n 25 -v 0
 
-python3 Optimisation.py -s 21 -z '[6 14]' -n 25 -v 0
-python3 Optimisation.py -s 21 -z '[6 14]' -n 10 -v 0
+
