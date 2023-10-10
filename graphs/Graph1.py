d2fea428fd23c2499e834f967085c803de65f356
Harry Thawley <hmthawley@gmail.com>
2023-10-11 09:05:17 +1100

    updated graphs

--- graphs/Graph1.py
+++ graphs/Graph1.py
@@ -18,11 +18,11 @@ import re
 
 #%%
 
-def lambdaZoneNames(scenario, stormZone=None, wdir=None):
-    if pd.isna(stormZone): return None
+def lambdaZoneNames(scenario, eventZone=None, wdir=None):
+    if pd.isna(eventZone): return None
     
-    pidx, widx, sidx, headers, stormZoneIndx = zoneTypeIndx(scenario, stormZone, wdir)
-    zoneNames = [name[7:-5] for name in np.array(headers[pidx:widx])[stormZoneIndx]]
+    pidx, widx, sidx, headers, eventZoneIndx = zoneTypeIndx(scenario, eventZone, wdir)
+    zoneNames = [name[7:-5] for name in np.array(headers[pidx:widx])[eventZoneIndx]]
     return zoneNames
 
 def readPrintedArray(txt):      
@@ -30,7 +30,7 @@ def readPrintedArray(txt):
     txt = re.sub(r"(?<!\[)\s+(?!\])", r",", txt)
     return np.array(literal_eval(txt), dtype =int)
 
-def zoneTypeIndx(scenario, stormZone=None, wdir=None):
+def zoneTypeIndx(scenario, eventZone=None, wdir=None):
     if wdir is not None: 
         active_dir = os.getcwd()
         os.chdir(wdir)
@@ -38,9 +38,9 @@ def zoneTypeIndx(scenario, stormZone=None, wdir=None):
     PVl   = np.array(['NSW']*7 + ['FNQ']*1 + ['QLD']*2 + ['FNQ']*3 + ['SA']*6 + ['TAS']*0 + ['VIC']*1 + ['WA']*1 + ['NT']*1)
     Windl = np.array(['NSW']*8 + ['FNQ']*1 + ['QLD']*2 + ['FNQ']*2 + ['SA']*8 + ['TAS']*4 + ['VIC']*4 + ['WA']*3 + ['NT']*1)
     names = np.char.replace(np.genfromtxt('Data\ZoneDict.csv', delimiter=',', dtype=str), 'ï»¿', '')
-    if isinstance(stormZone, str):        
-        if str(stormZone) == 'all': stormZone = ...
-        else: stormZone = readPrintedArray(stormZone) 
+    if isinstance(eventZone, str):        
+        if str(eventZone) == 'all': eventZone = ...
+        else: eventZone = readPrintedArray(eventZone) 
 
     if scenario<=17:
         node = Nodel[scenario % 10]
@@ -50,9 +50,9 @@ def zoneTypeIndx(scenario, stormZone=None, wdir=None):
         coverage = np.array([node])
         
         zones = np.zeros(Windl.shape)
-        zones[stormZone] = 1 
+        zones[eventZone] = 1 
         zones = zones[np.where(Windl==node)[0]]
-        stormZoneIndx = np.where(zones==1)[0]
+        eventZoneIndx = np.where(zones==1)[0]
         
     if scenario>=21:
         coverage = [np.array(['NSW', 'QLD', 'SA', 'TAS', 'VIC']),
@@ -65,9 +65,9 @@ def zoneTypeIndx(scenario, stormZone=None, wdir=None):
                     np.array(['FNQ', 'NSW', 'NT', 'QLD', 'SA', 'TAS', 'VIC', 'WA'])][scenario % 10 - 1]
         
         zones = np.zeros(Windl.shape)
-        zones[stormZone] = 1 
+        zones[eventZone] = 1 
         zones = zones[np.where(np.in1d(Windl, coverage)==True)[0]]
-        stormZoneIndx = np.where(zones==1)[0]
+        eventZoneIndx = np.where(zones==1)[0]
         
         names = names[np.where(np.in1d(np.append(PVl, Windl), coverage)==True)[0]]
         pzones = len(np.where(np.in1d(PVl, coverage)==True)[0])
@@ -81,24 +81,24 @@ def zoneTypeIndx(scenario, stormZone=None, wdir=None):
                ['storage-' + name + ' (GW)' for name in coverage] + 
                ['storage (GWh)'])
     
-    if stormZone is None: stormZoneIndx=None
+    if eventZone is None: eventZoneIndx=None
     
     if wdir is not None: 
         os.chdir(active_dir)
-    return pidx, widx, sidx, headers, stormZoneIndx
+    return pidx, widx, sidx, headers, eventZoneIndx
 
-def lambdaReadDeficit(scenario, zone, n_year):
-    try: return pd.read_csv(fr'S{scenario}-{zone}-{n_year}-{event}.csv')['StormDeficit'].sum()
+def lambdaReadDeficit(scenario, zone, n_year, event):
+    try: return pd.read_csv(fr'S{scenario}-{zone}-{n_year}-{event}.csv')['eventDeficit'].sum()
     except FileNotFoundError: 
         if n_year == -1: return 0
         else: return np.nan
 
-
+    
+    
 #%%
 if __name__ == '__main__':
     
-    event = 's' #storm
-    event = 'd' #drought
+    events = 's', 'd' 
     
     os.chdir('\\'.join(os.getcwd().split('\\')[:-1]))
     os.chdir('Results')
@@ -138,7 +138,7 @@ if __name__ == '__main__':
     
     data['GenerationAffected'] = 100*data['zone capacity'] / data['Total Gen']
     
-    data['Energy Deficit (TWh p.a.)'] = data[['Scenario', 'Zone', 'n_year']].apply(lambda row: lambdaReadDeficit(*row)*0.5/10/1e6, axis = 1)
+    data['Energy Deficit (TWh p.a.)'] = data[['Scenario', 'Zone', 'n_year', 'event']].apply(lambda row: lambdaReadDeficit(*row)*0.5/10/1e6, axis = 1)
     
     
     # data = pd.read_excel(r'Graph1DummyData.xlsx')
@@ -173,8 +173,9 @@ if __name__ == '__main__':
     # plt.legend()
     # plt.show()
     # plt.show()
-    
-    data['GridZone'] = data['Grid'] + data['Zone']
+    data = data[data['event'].isin(events)]
+    data = data[data['Scenario'] == 21]
+    data['GridZone'] = data['Grid'] + data['Zone'] + data['event']
     data1 = pd.melt(data
                     , id_vars = ['GridZone','Scenario', 'Zone Name', 'Zone', 'GenerationAffected']
                     , value_vars = ['LCOE ($/MWh)_relative', 'LCOG ($/MWh)_relative', 
@@ -184,48 +185,92 @@ if __name__ == '__main__':
     data1['GenerationAffected'] = data1['GenerationAffected'].round(1)
     
     fig, ax = plt.subplots(figsize = (12,4))
-    sns.barplot(data1#.loc[data1.loc[:,'Scenario'] == 21, :]
-                , x = 'GridZone'
-                , y = 'value'
-                , hue = 'variable'
-                )
+    g = sns.barplot(
+        data1#.loc[data1.loc[:,'Scenario'] == 21, :]
+        , x = 'GridZone'
+        , y = 'value'
+        , hue = 'variable'
+        )
     ax.axhline(0.0, linewidth = 0.5, color='black')
-    
-    # ax.set_xlim([None, 6])
+    g.legend_.remove()
+
+    ax.grid(True, 'major', 'y')
+    ax.set_xlim([None, 7.5])
+    ax.set_ylim([-2.5,2])
+    ax.set_yticks(np.arange(-5,5)/2.)
     ax.set_xlabel('Zone affected')
     ax.set_ylabel('Cost of resilient grid relative to ordinary')
-    ax.set_title('Relative costs by Storm Location')
-    plt.legend()
+    ax.set_title('Relative costs by event Location')
+
+    plt.figlegend()
     plt.show()
     
+#%%    
+    data2 = pd.melt(
+        data
+        , id_vars = ['GridZone', 'Scenario', 'Zone Name', 'Zone', 'GenerationAffected']
+        , value_vars = ['Solar (GW)_relative'
+                        , 'Wind (GW)_relative'
+                        # , 'Hydro & Bio (GW)_relative'
+                        , 'Pumped Hydro (GW)_relative'
+                        , 'Pumped Hydro (GWh)_relative'
+                        ])
     
-    data1 = pd.melt(data
-                    , id_vars = ['GridZone', 'Scenario', 'Zone Name', 'Zone', 'GenerationAffected']
-                    , value_vars = ['Solar (GW)_relative'
-                                    , 'Wind (GW)_relative'
-                                    # , 'Hydro & Bio (GW)_relative'
-                                    , 'Pumped Hydro (GW)_relative'
-                                    # , 'Pumped Hydro (GWh)_relative'
-                                    ])
-    data1['variable'] = data1['variable'].str.replace('_relative', '')
-    data1['GenerationAffected'] = data1['GenerationAffected'].round(1)
+    data2['variable'] = data2['variable'].str.replace('_relative', '')
+    data2['GenerationAffected'] = data2['GenerationAffected'].round(1)
     
     fig, ax = plt.subplots(figsize = (12,4))
-    sns.barplot(data1#.loc[data1.loc[:,'Scenario'] == 21, :]
-                , x = 'GridZone'
-                , y = 'value'
-                , hue = 'variable'
-                )
+    g1 = sns.barplot(
+        data2.loc[data2.loc[:,'variable'] != 'Pumped Hydro (GWh)', :]
+        , x = 'GridZone'
+        , y = 'value'
+        , hue = 'variable'
+        , ax = ax
+        )
     ax.axhline(0.0, linewidth = 0.5, color='black')
+    g1.legend_.remove()
     
-    # ax.set_xlim([None, 6])
+
+    ax2 = ax.twinx()
+    # g2 = sns.scatterplot(
+    #     data2.loc[data2.loc[:,'variable'] == 'Pumped Hydro (GWh)', :]
+    #     , x = 'GridZone'
+    #     , y = 'value'
+    #     , label = 'Pumped Hydro (GWh)'
+    #     , ax = ax2
+    #     , color = sns.color_palette()[3]
+    #     )
+    g2 = ax2.stem(
+        data2.loc[data2.loc[:,'variable'] == 'Pumped Hydro (GWh)', :]['GridZone']
+        , data2.loc[data2.loc[:,'variable'] == 'Pumped Hydro (GWh)', :]['value']
+        , label = 'Pumped Hydro (GWh)'
+        , basefmt=' '
+        , linefmt='r'
+        , markerfmt='r'
+        # , ax = ax2
+        # , color = sns.color_palette()[3]
+        )
+    
+    # g2.legend_.remove()
+
+    # align_yaxis(ax, 0, ax2, 0)
+
+    ax2.set_ylabel('Change to Energy Storage Capacity (GWh)')
+    
+    ax.grid(True, 'major', 'y')
+    # ax.set_xlim([None, 6.5])
+    ax.set_ylim([-10,20])
+    ax2.set_ylim([7.5*val for val in ax.get_ylim()])
+    ax2.set_yticks([7.5*val for val in ax.get_yticks()])
+    
+    ax.set_yticks(np.arange(-2,5)*5.0)    
     ax.set_xlabel('Zone affected')
-    ax.set_ylabel('Change to generation (GW)')
+    ax.set_ylabel('Change to Power Sources (GW)')
     ax.set_title('Relative Energy Mix by Location')
-    plt.legend()
+    plt.figlegend()
     plt.show()
     
-    
+#%%    
     os.chdir('\\'.join(os.getcwd().split('\\')[:-1]))
     os.chdir('graphs')
     
--- graphs/Graphs2-3.py
+++ graphs/Graphs2-3.py
@@ -15,12 +15,6 @@ import os
 import re
 from datetime import datetime as dt
 
-
-
-scenario = 21
-n_year = 25
-event = 's'
-
 def sfileMatch(file):
     try: 
         assert file[:4] == f'S{scenario}-'
@@ -32,6 +26,10 @@ def sfileMatch(file):
 def removeYear(date): return dt(2000, date.month, date.day, date.hour, date.minute)
 def lambdaDt(*args): return dt(*args)
 
+scenario = 21
+n_year = 25
+# event = 'd'
+
 
 os.chdir('\\'.join(os.getcwd().split('\\')[:-1]))
 os.chdir('Results')
@@ -41,47 +39,61 @@ ggtaSummary = pd.read_csv("GGTA-consolidated.csv")
 
 sfiles = [f for f in os.listdir() if sfileMatch(f)]
 
-for f in sfiles: 
-    if 'All' in f:  stormZone = 'All'
-    else: stormZone = '['+re.split('\[|\]', f)[1]+']'
+for event in ('d', 's'):
+    for f in sfiles: 
+        if 'All' in f:  eventZone = 'All'
+        else: eventZone = '['+re.split('\[|\]', f)[1]+']'
+        
+        data = pd.read_csv(f) 
+        data['eventEnergyDeficit'] = data['eventDeficit'] * 0.5
+        data['eventDeficit%'] = 100*data['eventDeficit'] / data['Operational demand (original)']
+        
+        fig, ax = plt.subplots() 
+        sns.histplot(data = data.loc[data['eventDeficit'] != 0, :]
+                     , x = 'eventDeficit%'
+                     , binwidth = 5
+                     , binrange = [0,data['eventDeficit%'].max()]
+                     )
+        ax.set_xlabel('Half Hourly Energy deficit (%)')
+        ax.set_ylabel('Occurences per decade')
+        ax.set_title(f'Frequency of energy deficits ({scenario}-{eventZone})')
+        plt.show()
+        
+        fig, ax = plt.subplots()
+        data['Date & time'] = pd.to_datetime(data['Date & time'], format='%a -%d %b %Y %H:%M')
     
-    data = pd.read_csv(f) 
-    data['StormEnergyDeficit'] = data['StormDeficit'] * 0.5
-    data['StormDeficit%'] = 100*data['StormDeficit'] / data['Operational demand (original)']
+        data['Day'] = data['Date & time'].dt.day
+        data['Month'] = data['Date & time'].dt.month
+        data = data.drop(columns = ['Date & time'])
+        data = (data.groupby(['Month', 'Day']).sum()/10).reset_index()
+        # data = (data.groupby(['Month', 'Day']).sum()).reset_index()
+        
+        data['Date'] = data[['Month','Day']].apply(lambda x: dt(2000, *x), axis = 1)
     
-    fig, ax = plt.subplots() 
-    sns.histplot(data = data.loc[data['StormDeficit'] != 0, :]
-                 ,x = 'StormDeficit%')
-    ax.set_xlabel('Half Hourly Energy deficit (%)')
-    ax.set_ylabel('Occurences per decade')
-    ax.set_title('Frequency of energy deficits')
-    plt.show()
+        # sns.lineplot(data = data
+        #                 , x = 'Date' 
+        #                 , y = 'eventDeficit%' 
+        #                 )
+        
+        ax.stem(
+            data['Date']
+            , data['eventDeficit%']
+            , basefmt= ' '
+            , markerfmt = ' '
+            )
+        ax.axhline(0.0, linewidth=1, color = sns.color_palette()[0])
     
-    fig, ax = plt.subplots()
-    data['Date & time'] = pd.to_datetime(data['Date & time'], format='%a -%d %b %Y %H:%M')
-
-    data['Day'] = data['Date & time'].dt.day
-    data['Month'] = data['Date & time'].dt.month
-    data = data.drop(columns = ['Date & time'])
-    data = (data.groupby(['Month', 'Day']).sum()/10).reset_index()
-    # data = (data.groupby(['Month', 'Day']).sum()).reset_index()
+        # ax.set_xticks([dt(2000, m, 1) for m in range(1,13,2)] + [dt(2000, 12, 31)])
+        ax.xaxis.set_major_formatter(pltd.DateFormatter('%d-%b'))
     
-    data['Date'] = data[['Month','Day']].apply(lambda x: dt(2000, *x), axis = 1)
-    sns.lineplot(data = data
-                    , x = 'Date' 
-                    , y = 'StormDeficit%' 
-                    )
-    # ax.set_xticks([dt(2000, m, 1) for m in range(1,13,2)] + [dt(2000, 12, 31)])
-    ax.xaxis.set_major_formatter(pltd.DateFormatter('%d-%b'))
-
-    ax.set_xlabel('Date (daily)')
-    ax.set_ylabel('Mean energy deficit (%)')
-    ax.set_title('Annual pattern of power deficits')
-    
-    # sns.scatterplot(data = data, 
-    #                 x = 'Date & time', 
-    #                 y = 'StormDeficit')
-    # ax.xaxis.set_major_formatter(pltd.DateFormatter('%Y'))
+        ax.set_xlabel('Date (daily)')
+        ax.set_ylabel('Mean energy deficit (%)')
+        ax.set_title(f'Annual pattern of power deficits ({scenario}-{eventZone}-{event})')
+        
+        # sns.scatterplot(data = data, 
+        #                 x = 'Date & time', 
+        #                 y = 'eventDeficit')
+        # ax.xaxis.set_major_formatter(pltd.DateFormatter('%Y'))
     
 
 os.chdir('\\'.join(os.getcwd().split('\\')[:-1]))
