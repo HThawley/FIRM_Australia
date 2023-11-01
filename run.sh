3aec85901e21281d6105c935ec91b1ee083cbbc9
TimWeberRE100 <timothy.weber@anu.edu.au>
2023-11-01 13:14:34 +1100

    -

--- run.sh
+++ run.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 
-python3 Dispatch.py -s 21 -z [7] -n 25 -e e 
-python3 Dispatch.py -s 21 -z [10] -n 25 -e e 
-python3 Dispatch.py -s 21 -z [14] -n 25 -e e 
+
 
 python3 Optimisation.py -s 21 -z None -n 25 -e None -x 2 -i 500 -t 1
 python3 Optimisation.py -s 21 -z None -n 25 -e None -x 2 -i 500 -t 2
