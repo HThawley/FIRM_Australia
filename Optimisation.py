20accc3d01f44f4c68491b8f2dc7a96272eddd65
Harry Thawley <Harry.Thawley@anu.edu.au>
2024-12-19 13:41:09 +1100

    improved multiprocessing

--- Optimisation.py
+++ Optimisation.py
@@ -5,32 +5,46 @@
 
 from scipy.optimize import differential_evolution
 from datetime import datetime as dt
+from multiprocessing import Pool
+from psutil import cpu_count
+
+ncpus = cpu_count(False)
 
 # from Costs import *
 from Input import *
 from Simulation import Reliability
 from Network import Transmission
 
+
 def F(x):
     S = Solution(x)
-    return Objective(S, costs)
+    S._evaluate(costs)
+    return S.LCOE + S.Penalties
+
+def mpWrapper(x):
+    with Pool(min(ncpus, x.shape[1])) as processPool:
+        result = processPool.imap(F, [xn for xn in x.T], chunksize=x.shape[1]//ncpus+1)
+        result = np.array([r for r in result])
+        processPool.terminate()
+    return result
 
 if __name__=='__main__':
     starttime = dt.now()
     print("Optimisation starts at", starttime)
 
     result = differential_evolution(
-        func=F, 
+        func=mpWrapper, 
         bounds=list(zip(lb, ub)), 
         tol=0,
         maxiter=args.i, 
         popsize=args.p, 
-        mutation=(0.2, args.m),
+        mutation=(0.3, args.m),
         recombination=args.r,
         disp=True, 
         polish=False, 
         updating='deferred', 
-        workers=-1,
+        # workers=-1,
+        vectorized=True,
         )
     try: 
         np.savetxt(f'Results/Optimisation_resultx{scenario}.csv', result.x.reshape(1,-1), fmt='%s', delimiter=',')
